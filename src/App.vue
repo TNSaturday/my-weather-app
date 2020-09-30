e13cce84b89b9df2524e20f786cabc6a02ef6898
TNsaturday <tnsaturday@gmail.com>
2020-09-30 21:10:08 +0300

    Try out installing material design rally preset

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,25 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-toolbar-title>
+        <v-btn
+        href="/"
+        target="_blank"
+        text
+      >
+        <span>Weather report</span>
+      </v-btn>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span>Sign In</span>
+        <v-icon>mdi-arrow-right</v-icon>
       </v-btn>
     </v-app-bar>
 
