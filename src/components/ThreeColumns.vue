2dde9206dcc7289950e19b0a3c32db750f6758c5
TNsaturday <tnsaturday@gmail.com>
2020-10-02 19:42:45 +0300

    Fix layout, add weather card

--- src/components/ThreeColumns.vue
+++ src/components/ThreeColumns.vue
@@ -1,29 +1,33 @@
 <template>
   <v-container>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8" lg="4">
-            <v-sheet rounded="lg" min-height="10vh">
-              <input color="grey" type="text" placeholder="Search">
-            </v-sheet>
-          </v-col>
-        </v-row>
 
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="8" lg="4">
+        <v-sheet rounded="lg" min-height="10vh">
+          <v-text-field
+        >
+          Search
+        </v-text-field>
+        </v-sheet>
+      </v-col>
+    </v-row>
 
-        <v-row align="center" justify="center">    
-          <v-col
-            cols="12"
-            sm="8"
-            md="8"
-            lg="4"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <WeatherCard />
-            </v-sheet>
-          </v-col>
-        </v-row>
+    <v-row align="center" justify="center">    
+      <v-col
+        cols="12"
+        sm="8"
+        md="8"
+        lg="4"
+      >
+        <v-sheet
+          min-height="70vh"
+          rounded="lg"
+        >
+          <WeatherCard />
+        </v-sheet>
+      </v-col>
+    </v-row>
+
       </v-container>
 </template>
 
