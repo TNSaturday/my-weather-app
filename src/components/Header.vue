0c41c13d0544617d2b1a74a07b62d358149372b7
TNsaturday <tnsaturday@gmail.com>
2020-10-03 16:49:27 +0300

    Add scroll to top functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <template>
   <v-app-bar app color="primary" dark>
     <v-toolbar-title>
-      <v-btn text>
+      <v-btn text @click.native="scrollToTop">
         <span>Прогноз погоды</span>
       </v-btn>
     </v-toolbar-title>
-
-    <v-spacer></v-spacer>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  name: 'Header',
+  methods: { 
+    scrollToTop() {
+      this.$store.commit('initState');
+      this.$router.push('/');
+      window.scrollTo(0,0);
+    }
+  }
+}
+</script>
